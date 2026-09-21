68ce39207d86a47c3712c35a503cb3f627c8a80a
24pa1a0518-specMallika <24pa1a0518@vishnu.edu.in>
2026-09-22 00:05:19 +0530

    Time: 1 ms (56.34%), Space: 43 MB (83.3%) - LeetHub

---
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        HashMap<Character,Integer> hm=new HashMap<>();
+        for(int i=0;i<stones.length();i++){
+            char key=stones.charAt(i);
+            hm.put(key,hm.getOrDefault(key,0)+1);
+        }
+        int ans=0;
+        for(int i=0;i<jewels.length();i++){
+            char ch=jewels.charAt(i);
+            if(hm.containsKey(ch)){
+                ans+=hm.get(ch);
+            }
+        }
+        return ans;
+    }
+}
