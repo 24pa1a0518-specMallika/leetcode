716d24e9e35455ff1c3441c487dc37c07efb5045
24pa1a0518-specMallika <24pa1a0518@vishnu.edu.in>
2026-09-23 00:42:15 +0530

    Time: 17 ms (88.43%), Space: 108.4 MB (23.12%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> hm=new HashSet<>();
+        for(int num:nums){
+            if(hm.contains(num)){
+                return true;
+            }
+            hm.add(num);
+        }
+        return false;
+    }
+}
